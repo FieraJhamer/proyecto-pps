afbca73f2845bac8e43bd726a8735709b2f03a77
Juan Ignacio Sotomayor <jignaciosotomayor205@gmail.com>
2025-01-22 12:08:36 -0300

    Se agrega el endpoint para hacer un get de los proyectos

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { conectarDB } from "./db.js";
 import usuarios from "./usuarios/usuarios.js";
 import router,{ authConfig } from "./usuarios/auth.js";
+import { proyectosRouter } from "./proyectos/proyectos.js";
 
 const app = express();
 const port = 3000;
@@ -15,6 +16,7 @@ authConfig();
 
 app.use("/", usuarios);
 app.use("/", router);
+app.use("/", proyectosRouter);
 
 // app.get("/usuarios", async (req, res) => {
 //   const [usuarios] = await db.execute("select * from usuarios");
---
+++ back/proyectos/proyectos.js
@@ -0,0 +1,21 @@
+import express from "express"
+import {db} from '../db.js'
+
+export const proyectosRouter = express.Router()
+
+proyectosRouter.get("/proyectos", async (req,res)=>{
+    const [proyectos]= await db.execute
+    (`SELECT 
+	p.nombre_proyecto, 
+	GROUP_CONCAT(CONCAT(a.nombre_alumno, ' ', a.apellido_alumno) SEPARATOR ', ') AS integrantes,
+    c.nombre_carrera as carreras
+                
+    FROM proyectos p 
+    JOIN alumnos a ON p.id_grupo = a.id_grupo
+
+    join grupos g on p.id_grupo = g.id_grupo
+
+    join carreras c on c.id_carrera=g.id_carrera
+    GROUP BY p.nombre_proyecto, c.nombre_carrera`)
+    res.send(proyectos)
+})
