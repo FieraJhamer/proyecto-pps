221c84a1866e160fb900ba46d35755f8df788330
Alejandro <gmz248alejandro@gmail.com>
2025-01-28 23:54:24 -0300

    Se agregaron validaciones para crearProyecto

--- back/proyectos/proyectos.js
+++ back/proyectos/proyectos.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { validarJwt } from "../validaciones/validaciones.js";
+
 import crearProyecto from "./crearProyecto.js";
+import { validarCrearProyecto } from "../validaciones/validaciones.js";
+
 import modificarProyecto from "./modificarProyecto.js"
 import obtenerProyecto from "./obtenerProyecto.js"
 
 const proyectos = express.Router();
 
-proyectos.post("/proyectos", validarJwt, crearProyecto);
+proyectos.post("/proyectos", validarJwt, validarCrearProyecto, crearProyecto);
 
 proyectos.get("/proyectos", validarJwt, obtenerProyecto);
 
